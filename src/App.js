f502c7480a2cda7b3d17f735250a1954b7451f67
nvmnghia <nvmnghia@gmail.com>
2022-04-06 16:17:08 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
   VStack,
-  Code,
-  Grid,
-  theme,
+  Input,
+  HStack,
+  PinInput,
+  PinInputField,
+  extendTheme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
+const components = {
+  Input: {
+    variants: {
+      outline: {
+        field: {
+          h: 88,
+          borderColor: 'red',
+        },
+      },
+    },
+  },
+
+  PinInput: {
+    variants: {
+      outline: {
+        field: {
+          h: 88,
+          borderColor: 'red',
+        },
+      },
+    },
+  },
+};
+
+const theme = extendTheme({ components });
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <VStack>
+        <Input />
+
+        <HStack>
+          <PinInput>
+            <PinInputField />
+            <PinInputField />
+            <PinInputField />
+          </PinInput>
+        </HStack>
+      </VStack>
     </ChakraProvider>
   );
 }
